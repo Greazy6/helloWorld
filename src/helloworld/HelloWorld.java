e24214951146365e3653103d581995a11e26c16f
Greazy6 <grclkrc@hotmail.com>
2015-09-28 13:22:19 -0400

    testing loops

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,6 +15,33 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("trying again");
         System.out.println("It worked");
+        
+        int x = 0;
+        int y = 0;
+        
+        while (x < 5){
+           /* y = y + 2;
+            if ( y > 4){
+                y = y -1;
+             */
+            /*x = x + 1;
+            y = y + x;
+            */
+            
+            if ( y < 5){
+                x = x + 1;
+                if (y < 3){
+                    x = x-1;
+                }
+            }
+            y = y + 2;
+            }
+            
+            
+            System.out.println( x + " " + y +  " ");
+            
+            x = x + 1;
+        }
+        
     }
     
-}
